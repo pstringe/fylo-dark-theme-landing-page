6f786ffa9c4357beb2836ab139bdd6f76607270b
Poitier Stringer <stringerpoitier@gmail.com>
2022-09-07 18:41:15 -0700

    update intro content

--- components/Sections/Intro.tsx
+++ components/Sections/Intro.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import styles from "@styles/Sections/Intro.module.scss";
 
+const HEADING_1 = `Freelancers Earn Top Dollar`;
+const HEADING_2 = `Comapanies Get Top Talent`
+const HEADING_3 = `No Middle Man`
+
+const SECTION_1_PARAGRAPH = "Limber connects companies with financial proffessionals. As a contractor keep 90%. of your earnings. As a company, get the best talent for your projects.";
+
 const Intro = () => (
   <section aria-labelledby="section1-title" className={styles.main}>
     <Image
@@ -11,13 +17,17 @@ const Intro = () => (
       objectFit="contain"
       priority
     />
-    <h1 id="section1-title">
-      All your files in one secure location, accessible anywhere.
+    <h3>
+      {HEADING_1}
+    </h3>
+    <h3>
+      {HEADING_2}
+    </h3>
+    <h1>
+      {HEADING_3}
     </h1>
     <p>
-      Fylo stores all your most important files in one secure location. Access
-      them wherever you need, share and collaborate with friends family, and
-      co-workers.
+      {SECTION_1_PARAGRAPH}
     </p>
     <button type="button">Get Started</button>
   </section>
