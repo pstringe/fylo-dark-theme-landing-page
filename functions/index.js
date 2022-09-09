17866d77ae653803c9513dc463d22be90fb61547
Poitier Stringer <stringerpoitier@gmail.com>
2022-09-09 16:38:49 -0700

    fix regex

--- functions/index.js
+++ functions/index.js
@@ -30,19 +30,18 @@ async function addEmail(email) {
 }
 
 function validateEmail(email) {
-  const re =
-    /^(([^&lt;&gt;()[\\]\\\\.,;:\\s@&quot;]+(\\.[^&lt;&gt;()[\\]\\\\.,;:\\s@&quot;]+)*)|(&quot;.+&quot;))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+    const emailRegex = new RegExp(
+        /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+      );
+  return emailRegex.test(String(email).toLowerCase());
 }
 
 module.exports.handler = async function (event, context) {
-    console.log('in func')
     // Check the request method
     if (event.httpMethod != 'POST') {
         return { statusCode: 405, body: 'Method not allowed' };
     }
 
-    console.log('check method', {event})
     // Get the body
     try {
         var body = JSON.parse(event.body);
@@ -54,14 +53,12 @@ module.exports.handler = async function (event, context) {
         return;
     }
 
-    console.log('got bod')
     // Validate the email
     const { email } = body;
     if (!validateEmail(email)) {
         return { statusCode: 400, body: 'Email is not valid' };
     }
 
-    console.log('validate email')
     // Store email in Notion
     await addEmail(email);
     return { statusCode: 200, body: 'ok' };
